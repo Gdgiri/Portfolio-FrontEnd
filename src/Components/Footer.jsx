8d10b4409b53afd47b5d4da10ccfb66d17cd0a97
giridharan <giridharan831@gmail.com>
2024-10-24 14:50:11 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {
-  FaFacebookF,
-  FaGithub,
-  FaLinkedinIn,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
@@ -73,28 +69,28 @@ const Footer = () => {
         {/* Right Side - Social Media Icons */}
         <div className="flex space-x-6">
           <a
-            href="#"
-            aria-label="Facebook"
-            className="text-blue-500 hover:text-blue-500 transition-transform transform hover:scale-125"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=giridharan831@gmail.com"
+            aria-label="Gmail"
+            className="text-red-600 hover:text-red-600 transition-transform transform hover:scale-125"
           >
-            <FaFacebookF className="w-6 h-6" />
+            <SiGmail className="w-6 h-6" />
           </a>
           <a
-            href="#"
+            href="https://github.com/Gdgiri"
             aria-label="GitHub"
             className="text-gray-900 hover:text-gray-900 transition-transform transform hover:scale-125"
           >
             <FaGithub className="w-6 h-6" />
           </a>
           <a
-            href="#"
+            href="https://www.linkedin.com/in/giridharan-sivaramakrishnan-91353b333"
             aria-label="LinkedIn"
             className="text-blue-500 hover:text-blue-700 transition-transform transform hover:scale-125"
           >
             <FaLinkedinIn className="w-6 h-6" />
           </a>
           <a
-            href="#"
+            href="https://wa.me/9715424895"
             aria-label="WhatsApp"
             className="text-green-600 hover:text-green-600 transition-transform transform hover:scale-125"
           >
