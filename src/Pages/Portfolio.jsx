f5ec0744e346f8c93b5fdf53c3142b7f9dca207e
giridharan <giridharan831@gmail.com>
2024-10-25 13:03:46 +0530

    update-portfolio

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
       title: "GD-Events",
       description: "GD-Events is used to book the venues for marriage.",
       imageUrl:
-        "https://github.com/user-attachments/assets/c819cbe1-3451-4c97-8a3a-9f1f84bca852",
+        "https://github.com/user-attachments/assets/e147e2ab-c098-4c12-86ad-97fe6e817e8c",
       projectLink: "https://wedding-app-gdevents.netlify.app/",
     },
     {
@@ -47,23 +47,28 @@ const Portfolio = () => {
             key={project.id}
             className="overflow-hidden rounded-lg shadow-xl transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl bg-white"
           >
-            <img
-              src={project.imageUrl}
-              alt={project.title}
-              className="w-full h-80  transition-transform transform hover:scale-110" // Set a fixed height
-            />
+            <div className="h-64 w-full relative">
+              {/* Set height for the image wrapper */}
+              <img
+                src={project.imageUrl}
+                alt={project.title}
+                className="w-full h-full object-cover transition-transform transform hover:scale-110" // Ensure the image covers the full height and width
+              />
+            </div>
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800">
-              Project-Title: {project.title}
+                Project-Title: {project.title}
               </h3>
-              <p className="text-gray-600 mb-2"><strong>Project-Description:</strong> {project.description}</p>
+              <p className="text-gray-600 mb-2">
+                <strong>Project-Description:</strong> {project.description}
+              </p>
               {project.projectLink && (
                 <div className="text-center">
                   <a
                     href={project.projectLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-block mt-4 px-6 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-all duration-300 "
+                    className="inline-block mt-4 px-6 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-all duration-300"
                   >
                     View Project
                   </a>
